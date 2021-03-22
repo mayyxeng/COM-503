7993730433ba9dadd78ab0d9d34698f891f7d424
rishabh246 <rishabh246@gmail.com>
2021-03-22 21:41:35 +0000

    Checkpoint part 4

--- homework-2/scripts/part2.py
+++ homework-2/scripts/part2.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import matplotlib
+import statistics
 
 
 class RandomWaypoint:
@@ -116,33 +117,32 @@ class RandomWaypoint:
         fig.savefig('../figures/event_view_' + str(num_users) + '.pdf')
         plt.show()
     
+    def get_time_series(self, user_id, t_step, num_steps):
+        ## Assumption is that start time is 0
+        time = 0
+        state_ctr = 0
+        time_series = list()
+        states = self.z[user_id]
+        for i in range(0, num_steps):
+            time = time + t_step
+            while(states[state_ctr].t < time):
+                state_ctr = state_ctr + 1
+            state_ctr = state_ctr - 1
+            time_fraction = (time - states[state_ctr].t)/(states[state_ctr+1].t - states[state_ctr].t)
+            x = states[state_ctr].m.x + (states[state_ctr+1].m.x - states[state_ctr].m.x)* time_fraction
+            y = states[state_ctr].m.y + (states[state_ctr+1].m.y - states[state_ctr].m.y)* time_fraction
+            v = states[state_ctr].v
+            time_series.append(RandomWaypoint.State(RandomWaypoint.Coord(x,y),time,v))
+        
+        return time_series
 
     def plot_time_view(self, num_users):
-
-        def get_time_series(states, t_step, num_steps):
-            ## Assumption is that start time is 0
-            time = 0
-            state_ctr = 0
-            time_series = list()
-            for i in range(0, num_steps):
-                time = time + t_step
-                while(states[state_ctr].t < time):
-                    state_ctr = state_ctr + 1
-                state_ctr = state_ctr - 1
-                time_fraction = (time - states[state_ctr].t)/(states[state_ctr+1].t - states[state_ctr].t)
-                x = states[state_ctr].m.x + (states[state_ctr+1].m.x - states[state_ctr].m.x)* time_fraction
-                y = states[state_ctr].m.y + (states[state_ctr+1].m.y - states[state_ctr].m.y)* time_fraction
-                v = states[state_ctr].v
-                time_series.append(RandomWaypoint.State(RandomWaypoint.Coord(x,y),time,v))
-            
-            return time_series
-        
         fig, axs = plt.subplots(2)
         velocities = list()
         x = list()
         y = list()
-        for user_states in self.z[0:num_users]:
-                time_series = get_time_series(user_states,10,8639)
+        for user_id in range(0,num_users):
+                time_series = self.get_time_series(user_id,10,8639)
                 for _s in time_series:
                     velocities.append(_s.v)
                     x.append(_s.m.x)
@@ -161,6 +161,71 @@ class RandomWaypoint:
         fig.savefig('../figures/time_view_' + str(num_users) + '.pdf')
         plt.show()
 
+    def conf_interval(self, num_users):
+
+        def median_95_conf_interval_indices(num_samples) -> (int,int):
+            if(num_samples == 30):
+                return (10,21)
+            elif (num_samples > 70):
+                j = int(0.5*num_samples - 0.98*np.sqrt(num_samples))
+                k = int(0.5*num_samples + 1 + 0.98*np.sqrt(num_samples))
+                return (j,k)
+            else:
+                assert(0 and "num_samples not supported")
+
+        event_avg_speed = list()
+        time_avg_speed = list()
+        for user_id in range(0,num_users):
+            event_speeds = [_s.v for _s in self.z[user_id]]
+            time_speeds = [_s.v for _s in self.get_time_series(user_id,10,8639)]
+            event_avg_speed.append(sum(event_speeds)/len(event_speeds))
+            time_avg_speed.append(sum(time_speeds)/len(time_speeds))
+        
+        event_avg_speed.sort()
+        time_avg_speed.sort()
+        j, k = median_95_conf_interval_indices(num_users)
+
+        event_mean = sum(event_avg_speed)/len(event_avg_speed)
+        event_var = np.var(event_avg_speed)
+        time_mean = sum(time_avg_speed)/len(time_avg_speed)
+        time_var = np.var(time_avg_speed)
+        event_interval = 1.96*np.sqrt(event_var)/np.sqrt(num_users)
+        time_interval = 1.96*np.sqrt(time_var)/np.sqrt(num_users)
+
+        print("** Median for %d users ** " %(num_users))
+        print("%f %f %f"  %(statistics.median(event_avg_speed), event_avg_speed[j], event_avg_speed[k]))
+        print("%f %f %f" %(statistics.median(time_avg_speed), time_avg_speed[j], time_avg_speed[k]))
+        print("** Mean for %d users ** " %(num_users))
+        print("%f %f %f" %(event_mean, event_mean-event_interval, event_mean+event_interval))
+        print("%f %f %f" %(time_mean, time_mean-time_interval, time_mean+time_interval))
+
+    def pred_interval(self, num_users):
+
+        def order_statistic_95_indices(num_samples) -> (int,int):
+            if(num_samples == 30):
+                return (0,0)
+            elif (num_samples >= 60):
+                j = int((num_samples+1)*0.025)
+                k = int((num_samples+1)*0.975)
+                return (j,k)
+            else:
+                assert(0 and "num_samples not supported")
+        
+        time_avg_speed = list()
+        for user_id in range(0,num_users):
+            time_speeds = [_s.v for _s in self.get_time_series(user_id,10,8639)]
+            time_avg_speed.append(sum(time_speeds)/len(time_speeds))
+        
+        time_avg_speed.sort()
+        j, k = order_statistic_95_indices(num_users)
+        time_mean = sum(time_avg_speed)/len(time_avg_speed)
+        time_var = np.var(time_avg_speed)
+        time_interval = 1.96*np.sqrt(time_var)
+
+        print("** Prediction interval for %d users ** " %(num_users))
+        print("Assuming normal data\n %f - %f" %(time_mean-time_interval, time_mean+time_interval))
+        if(num_users > 39):
+            print("Assuming order statistic\n %f - %f" %(time_avg_speed[j], time_avg_speed[k]))
 
 if __name__ == "__main__":
 
@@ -180,4 +245,9 @@ if __name__ == "__main__":
     sim_obj.plot_event_view(1)
     sim_obj.plot_event_view(sim_obj.num_users)
     sim_obj.plot_time_view(1)
-    sim_obj.plot_time_view(sim_obj.num_users)
+    # sim_obj.plot_time_view(sim_obj.num_users)
+    sim_obj.conf_interval(150)
+    sim_obj.conf_interval(30)
+    sim_obj.pred_interval(150)
+    sim_obj.pred_interval(60)
+    sim_obj.pred_interval(30)
